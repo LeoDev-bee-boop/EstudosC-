0dd3056ce2c9b5662b8c3443cf886b5e4e13e6b7
Leonardo Bratfisch <sevalhosleonardo@gmail.com>
2022-01-27 23:54:05 -0300

    Custom_Controls | Acompanhando até 7:11 da aula, link: https://youtu.be/m7Iv6xfjnuw

--- Custom_Controls/Form1.Designer.cs
+++ Custom_Controls/Form1.Designer.cs
@@ -29,13 +29,38 @@ namespace Custom_Controls
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rjToggleButton1 = new Custom_Controls.RJControls.RJToggleButton();
+            this.SuspendLayout();
+            // 
+            // rjToggleButton1
+            // 
+            this.rjToggleButton1.AutoSize = true;
+            this.rjToggleButton1.Checked = true;
+            this.rjToggleButton1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.rjToggleButton1.Location = new System.Drawing.Point(497, 84);
+            this.rjToggleButton1.MinimumSize = new System.Drawing.Size(45, 22);
+            this.rjToggleButton1.Name = "rjToggleButton1";
+            this.rjToggleButton1.Size = new System.Drawing.Size(101, 22);
+            this.rjToggleButton1.TabIndex = 0;
+            this.rjToggleButton1.Text = "rjToggleButton1";
+            this.rjToggleButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rjToggleButton1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private RJControls.RJToggleButton rjToggleButton1;
     }
 }
 
