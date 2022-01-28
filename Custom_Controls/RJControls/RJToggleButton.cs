572bef9593491fe7ba64fe8ccc17c634be5d8816
Leonardo Bratfisch <leonardobratfisch@gmail.com>
2022-01-28 14:38:39 -0300

    Custom_Controls | Ajuste no RJToggleButton

--- Custom_Controls/RJControls/RJToggleButton.cs
+++ Custom_Controls/RJControls/RJToggleButton.cs
@@ -26,14 +26,17 @@ namespace Custom_Controls.RJControls
         //Methods
         private GraphicsPath GetFigurePath()
         {
+            
+            //Declarando curvatura
             int arcSize = this.Height - 1;
+            //Declarando e arredondando os cantos do retângulo
             Rectangle leftArc = new Rectangle(0, 0, arcSize, arcSize);
             Rectangle rightArc = new Rectangle(this.Width - arcSize - 2, 0, arcSize, arcSize);
 
             GraphicsPath path = new GraphicsPath();
             path.StartFigure();
-            path.AddArc(leftArc, 90, 180);
-            path.AddArc(rightArc, 270, 180);
+            path.AddArc(leftArc,90,180);
+            path.AddArc(rightArc,270,180);
             path.CloseFigure();
 
             return path;
@@ -41,7 +44,7 @@ namespace Custom_Controls.RJControls
 
         protected override void OnPaint(PaintEventArgs pevent)
         {
-            int toggleSize = this.Height = 5;
+            int toggleSize = this.Height - 5;
             pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
             pevent.Graphics.Clear(this.Parent.BackColor);
 
@@ -50,8 +53,13 @@ namespace Custom_Controls.RJControls
                 //Draw the control surface
                 pevent.Graphics.FillPath(new SolidBrush(onBackColor), GetFigurePath());
                 //Draw the toggle
+                pevent.Graphics.FillEllipse(new SolidBrush(onToggleColor), new Rectangle(this.Width - this.Height + 1 , 2, toggleSize, toggleSize));
+            } else
+            {
+                //Draw the control surface
+                pevent.Graphics.FillPath(new SolidBrush(offBackColor), GetFigurePath());
+                //Draw the toggle
                 pevent.Graphics.FillEllipse(new SolidBrush(offToggleColor), new Rectangle(2, 2, toggleSize, toggleSize));
-                    
             }
         }
     }
