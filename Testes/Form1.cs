42c3d27081c61bf562cfc9c0e6133935922b9741
Leonardo Bratfisch <leonardobratfisch@gmail.com>
2022-02-08 13:54:54 -0300

    Testes | Instânciando método que grava email digitado 'btnGravar_Click'.

--- Testes/Form1.cs
+++ Testes/Form1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +21,18 @@ namespace Testes
 
         private void btnGravar_Click(object sender, EventArgs e)
         {
+            if (File.Exists(CaminhoBaseEmail()))
+            {
+                StreamWriter sr = new StreamWriter(CaminhoBaseEmail());
+                string email = cbEmail.Text;
+                sr.WriteLine(email);
+                sr.Close();
+            }
+        }
 
+        private static string CaminhoBaseEmail()
+        {
+            return ConfigurationManager.AppSettings["CaminhoEmail"];
         }
     }
 }
